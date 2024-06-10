2684f3e808120c8bc511aac7a0cc566a37e282e4
Bahir Hussain <mbahirh624@gmail.com>
2024-06-10 10:52:09 +0530

    Deposit money added

--- src/main/java/com/bankingapplication/banking_app/controller/AccountController.java
+++ src/main/java/com/bankingapplication/banking_app/controller/AccountController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 
+import java.util.Map;
+
 import static org.springframework.http.HttpStatus.*;
 
 @RestController
@@ -29,6 +31,14 @@ public class AccountController {
     @GetMapping("/{id}")
     public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id){
         AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    //Deposit REST API
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<AccountDto> deposit(@PathVariable Long id, @RequestBody Map<String, Double> request){
+        Double amount = request.get("amount");
+        AccountDto accountDto = accountService.deposit(id, amount);
         return ResponseEntity.ok(accountDto);
     }
 }
--- src/main/java/com/bankingapplication/banking_app/service/impl/AccountServiceImpl.java
+++ src/main/java/com/bankingapplication/banking_app/service/impl/AccountServiceImpl.java
@@ -34,4 +34,13 @@ public class AccountServiceImpl implements AccountService {
         Account  account = accountRepository.findById(id).orElseThrow(() -> new RuntimeException("Account doesn't Exists"));
         return AccountMapper.mapToAccountDto(account);
     }
+
+    @Override
+    public AccountDto deposit(Long id, double amount) {
+        Account  account = accountRepository.findById(id).orElseThrow(() -> new RuntimeException("Account doesn't Exists"));
+        double total = account.getBalance() + amount;
+        account.setBalance(total);
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
 }
